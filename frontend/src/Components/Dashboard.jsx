ebc2b26f2d3ebf21c22659905aec1d285579c711
Mitch Lenaerts <lenaerts.mitch@gmail.com>
2026-03-28 14:08:51 +0100

    ApplicationsTableComponent aangemaakt (statisch) + css opmaak

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { getDashboardKpis } from "../Services/dashboardService";
 import { MapKPIs } from "../mappers/kpiMapper";
 import StatCard from "./StatCard";
+import ApplicationsTable from "./ApplicationsTable";
+import { Solicitaties } from "../dummy";
 export default function Dashboard() {
     const [kpis, setKpis] = useState([])
 
@@ -16,19 +18,25 @@ export default function Dashboard() {
         fetchKpis()
     }, [])
 
-    const KPIElements = kpis.map((kpi) => 
-            <StatCard key={kpi.id}
-            label= {kpi.label}
-            waarde= {kpi.value}
-            icoon = {kpi.icon}
-            kleur = {kpi.color}
-            />)
+    const KPIElements = kpis.map((kpi) =>
+        <StatCard key={kpi.id}
+            label={kpi.label}
+            waarde={kpi.value}
+            icoon={kpi.icon}
+            kleur={kpi.color}
+        />)
     return (
         <main className="dashboard-container">
-            <h1>Dashboard</h1>
-            <p>Overzicht van je lopende sollicitaties</p>
+            <h1 className="dashboard-title">Dashboard</h1>
+            <p className="dashboard-subtitle">Overzicht van je lopende sollicitaties</p>
             <div className="kpi-container">
                 {KPIElements}
+            </div>
+            <div className="display-bord">
+                <div className="applicationsTable-container">
+                    <h2 className="applicationsTable-title">Lopende sollicitaties</h2>
+                    <ApplicationsTable applications={Solicitaties} />
+                </div>
             </div>
         </main>
     )
